e9b7fddfcd3477ea226c2ea3171f4d623e83a665
abubaker <mohammad_abubaker@yahoo.com>
2021-02-07 15:20:09 +0500

    Added LiveData & Observer in ScoreFragment.kt and ScoreViewModel.kt

--- app/src/main/java/com/example/android/guesstheword/screens/score/ScoreViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/score/ScoreViewModel.kt
@@ -1,16 +1,21 @@
 package com.example.android.guesstheword.screens.score
 
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class ScoreViewModel(finalScore: Int) : ViewModel() {
 
     // The final score
-    var score = finalScore
+    private val _score = MutableLiveData<Int>()
+    val score: LiveData<Int>
+        get() = _score
 
     // Equivalent to onCreateView() in the UI Controller
     init {
-        Log.i("ScoreViewModel", "I received $finalScore from GameFragment.kt and stored it in the score with the value of $score")
+        // Log.i("ScoreViewModel", "I received $finalScore from GameFragment.kt and stored it in the score with the value of $score")
+        _score.value = finalScore
     }
 
     override fun onCleared() {
